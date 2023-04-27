87838d07e8d31185b22a227bffc0eeaa1ab26a86
kushvarma-solute <kush.v@solutelabs.com>
2023-04-27 14:46:23 +0530

    thirdd

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log("starttt");
+  console.log("serverrt");
   await app.listen(3000);
 }
 bootstrap();
